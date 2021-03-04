88d1f8743933d8b434e683400acf92f42153289d
Jessica Perez <1jessicaperez@gmail.com>
2021-03-04 15:28:58 -0500

    AuthActions added

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -9,10 +9,11 @@ import {
 
 import AuthService from '../services/auth.service';
 
+// REGISTER =====================
 export const register = (username, email, password) => (dispatch) => {
     return AuthService.register(username, email, password)
     .then ((response) => {
-        console.log('RESPONSE', response)
+        // console.log('RESPONSE', response)
         dispatch({
             type: REGISTER_SUCCESS
         });
@@ -28,7 +29,9 @@ export const register = (username, email, password) => (dispatch) => {
             const message = (error.response && 
                 error.response.data && 
                 error.response.data.message) || error.message || error.toString();
-
+                dispatch ({
+                    type: REGISTER_FAIL
+                });
                 dispatch ({
                     type: SET_ALERT,
                     payload: message
@@ -36,3 +39,34 @@ export const register = (username, email, password) => (dispatch) => {
                 return Promise.reject();
     });
 };
+// LOGIN =====================
+export const login = (username, password) => (dispatch) => {
+    return AuthService.login(username, password)
+    .then((data) => {
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: {user:data}
+        });
+        return Promise.resolve()
+    }
+    (error) => {
+        const message = (error.response &&
+            error.response.data &&
+            error.response.data.message) || error.message || error.toString();
+                dispatch ({
+                    type: LOGIN_FAIL
+                });
+                dispatch ({
+                    type: SET_ALERT,
+                    payload: message
+                });
+                return Promise.reject();
+    });
+};
+//LOGOUT ======================
+export const logout = () => (dispatch) => {
+    AuthService.logout();
+    dispatch({
+        type: LOGOUT
+    });
+}
