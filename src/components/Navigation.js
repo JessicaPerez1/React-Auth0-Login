af92e05ef31d3d1bf0692c86366a994a1ac9475d
Jessica Perez <1jessicaperez@gmail.com>
2021-03-04 15:20:30 -0500

    Navbar added

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
+import { logout } from '../services/auth.service';
+import { useDispatch, useSelector } from 'react-redux'
 
 const Navigation = () => {
+    const history = useHistory()
+    // const { user: currentUser } = useSelector((state) => state.auth)
+    const dispatch = useDispatch()
+    const [show, setShow] = useState(false);
+
+    const logOut = () => { dispatch(logout());
+  };
     return (
-        <h1>Navbar COMPONENT</h1>
+        <nav className='navbar-container'>
+            <Link to='/'> Home</Link>
+            { currentUser ? (
+                <button
+                className='logout-btn'
+                onClick={() => {
+                    logOut();
+                    history.push('/')
+                }}>Logout</button>
+            ) : (
+                <Link to='/login'>Login</Link>
+            ) }
+        </nav>
     )
 }
 
